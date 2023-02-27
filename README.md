7e22aa9c6fa489a651e63dbf14df93e97b61662d
juliedosher <juliegdosher@lewisu.edu>
2023-02-27 11:50:52 -0600

    added template function from epogue's github

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # HealthInsuranceRiskCalculatorServer
+[LINK]  
+  
+Authors: Alex, Julie, Le, Soren, Vy  
+  
+Credit:  
+Followed tutorial for creating server side functions:   
+ https://dev.to/gbudjeakp/how-to-connect-your-client-side-to-your-server-side-using-node-and-express-2i71  
+  
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// TODO
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3000
+
+
+// We are using our packages here
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+ extended: true})); 
+app.use(cors())
+
+//You can use this to check if your server is working
+app.get('/', (req, res)=>{
+res.send("Welcome to your server")
+})
+
+// below function from epogue's github:
+// Template for calculating BMI using height in feet/inches and weight in pounds.
+app.get('/calculate-bmi', (request, response) => {
+	console.log('Calling "/calculate-bmi" on the Node.js server.')
+	var inputs = url.parse(request.url, true).query
+	const heightFeet = parseInt(inputs.feet)
+	const heightInches = parseInt(inputs.inches)
+	const weight = parseInt(inputs.lbs)
+
+	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
+	console.log('Weight:' + weight + ' lbs.')
+
+	// Todo: Implement unit conversions and BMI calculations.
+	// Todo: Return BMI instead of Todo message.
+
+	response.type('text/plain')
+	response.send('Todo: Implement "/calculate-bmi"')
+})
+
+
+//Start your server on a specified port
+app.listen(port, ()=>{
+    console.log(`Server is runing on port ${port}`)
+})
