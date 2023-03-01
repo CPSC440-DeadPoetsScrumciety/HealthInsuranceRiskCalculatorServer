8b1242c256dd620fef992f4312a6ef99a15cb8b0
juliedosher <juliegdosher@lewisu.edu>
2023-03-01 10:22:18 -0600

    updated comments

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors')
 const port = 3000
 
 
-// We are using our packages here
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
  extended: true})); 
+ 
 app.use(cors())
 
-//You can use this to check if your server is working
+
 app.get('/', (req, res)=>{
 res.send("Welcome to your server")
 })
