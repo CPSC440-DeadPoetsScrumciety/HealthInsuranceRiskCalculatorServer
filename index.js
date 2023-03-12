cce9cb6b3cd12796e67acfb97881bc920ce11c56
Julie Dosher <112606564+juliedosher@users.noreply.github.com>
2023-03-12 18:38:58 -0500

    added origin, removed header

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-this.app.use(cors());
+this.app.use(cors({
+   	origin: "https://dps-insuranceriskcalculator.azurewebsites.net"
+}));
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  });
 
 app.get('/', (req, res) => {
 	res.type('text/plain')
